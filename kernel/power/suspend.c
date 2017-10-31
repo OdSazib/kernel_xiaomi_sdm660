3fa130e5523d75f6f315bbe9572e9b14941240e9
Rajat Jain <rajatja@google.com>
2017-10-31 14:44:24 -0700

    PM / s2idle: Clear the events_check_enabled flag
    [ Upstream commit 95b982b45122c57da2ee0b46cce70775e1d987af ]
    
    Problem: This flag does not get cleared currently in the suspend or
    resume path in the following cases:
    
     * In case some driver's suspend routine returns an error.
     * Successful s2idle case
     * etc?
    
    Why is this a problem: What happens is that the next suspend attempt
    could fail even though the user did not enable the flag by writing to
    /sys/power/wakeup_count. This is 1 use case how the issue can be seen
    (but similar use case with driver suspend failure can be thought of):
    
     1. Read /sys/power/wakeup_count
     2. echo count > /sys/power/wakeup_count
     3. echo freeze > /sys/power/wakeup_count
     4. Let the system suspend, and wakeup the system using some wake source
        that calls pm_wakeup_event() e.g. power button or something.
     5. Note that the combined wakeup count would be incremented due
        to the pm_wakeup_event() in the resume path.
     6. After resuming the events_check_enabled flag is still set.
    
    At this point if the user attempts to freeze again (without writing to
    /sys/power/wakeup_count), the suspend would fail even though there has
    been no wake event since the past resume.
    
    Address that by clearing the flag just before a resume is completed,
    so that it is always cleared for the corner cases mentioned above.
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -387,7 +387,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 			error = suspend_ops->enter(state);
 			trace_suspend_resume(TPS("machine_suspend"),
 				state, false);
-			events_check_enabled = false;
 		} else if (*wakeup) {
 			error = -EBUSY;
 		}
@@ -533,6 +532,7 @@ static int enter_state(suspend_state_t state)
 	pm_restore_gfp_mask();
 
  Finish:
+	events_check_enabled = false;
 	pr_debug("PM: Finishing wakeup.\n");
 	suspend_finish();
  Unlock:
