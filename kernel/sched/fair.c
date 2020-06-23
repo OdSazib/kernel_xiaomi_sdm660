8363122b0fcbf33f2f13f819af33289db5ec771f
Tyler Nijmeh <tylernij@gmail.com>
2020-06-22 21:34:59 -0700

    sched/sysctl: Further tweak the scheduler
    kernel.sched_child_runs_first: 0 --> 1
    When forking a child process from the parent, execute the child process before the parent process. This usually shaves down some latency on task initializations, since most of the time the child process is doing some form of heavy lifting.
    
    kernel.sched_nr_migrate: 32 --> 128
    When migrating tasks between CPUs, allow the scheduler to migrate twice as many as usual. This should increase scheduling latency marginally, but increase the performance of SCHED_OTHER tasks. In testing, cyclictest reported a 2 microsecond increase in average latency, an a decrease in maximum latency of SCHED_FIFO tasks.
    
    kernel.sched_min_granularity_ns: 2500000 (2.5ms)
    Set the minimum task scheduling period to 2.5ms. With kernel.sched_latency_ns set to 2.5ms, this means that 4 active tasks may execute within the 10ms scheduling period before we exceed it. Originally, this value was set to 1ms. After benchmarking using hackbench -pl 8000, it was determined that a higher value reduces hackbench times significantly. The tradeoff is scheduling latency.
    
    kernel.sched_wakeup_granularity_ns: 1000000 (1ms) --> 10000000 (10ms)
    Require the current task to be surpassing the new task in vmruntime by 10ms instead of 1ms before preemption occurs. In testing, hackbench -pl 8000 times were reduced by ~94% (NOT a typo).
    
    kernel.sched_min_task_util_for_boost: 35
    This value effects if tasks should be migrated to a higher performant CPU if it's utilization is above this amount (during sched_boost). Allow tasks to be migrated upwards if the user is triggering a touch boost and the task is above 35% utilization.
    
    kernel.sched_min_task_util_for_colocation: 25
    This value is the same as the former, except it occurs only for top-app tasks (which are of greater priority than background tasks). Lower this value a bit to use big clusters more for top-app tasks.
    
    [ghostrider-reborn] taken from tytydraco:
    
    Change-Id: Id34eb5e8eb379e747e924111bfe78a856a6990f6
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -105,8 +105,8 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_N
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 1000000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 1000000ULL;
+unsigned int sysctl_sched_min_granularity		= 2500000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 2500000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
@@ -128,8 +128,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
  *
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity		= 5000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 5000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 10000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 10000000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -345,13 +345,6 @@ static int max_extfrag_threshold = 1000;
 #endif
 
 static struct ctl_table kern_table[] = {
-	{
-		.procname	= "sched_child_runs_first",
-		.data		= &sysctl_sched_child_runs_first,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
-	},
 #if defined(CONFIG_PREEMPT_TRACER) && defined(CONFIG_PREEMPTIRQ_EVENTS)
 	{
 		.procname       = "preemptoff_tracing_threshold_ns",
@@ -636,15 +629,6 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
-	},
-	{
-		.procname	= "sched_wakeup_granularity_ns",
-		.data		= &sysctl_sched_wakeup_granularity,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= sched_proc_update_handler,
-		.extra1		= &min_wakeup_granularity_ns,
-		.extra2		= &max_wakeup_granularity_ns,
 	},
 #ifdef CONFIG_SMP
 	{
