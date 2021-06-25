0a4e607f16207236ddf10cd5a8d4731d79b7cc24
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-06-25 18:02:38 +0700

    drivers/fbdev: mdss: import from xiaomi changes
    *to enable some display commands, such as srgb
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -28,6 +28,10 @@
 #define VSYNC_DELAY msecs_to_jiffies(17)
 
 #ifdef CONFIG_MACH_LONGCHEER
+extern int first_ce_state, first_cabc_state, first_srgb_state, first_gamma_state, first_cabc_movie_state, first_cabc_still_state;
+extern int mdss_first_set_feature(struct mdss_panel_data *pdata, int first_ce_state, int first_cabc_state, int first_srgb_state, int first_gamma_state,
+		int first_cabc_movie_state, int first_cabc_still_state);
+extern bool first_set_bl;
 bool tianma_jdi_flag=0;
 char g_lcd_id[128];
 struct mdss_dsi_ctrl_pdata *ctrl_pdata_whitepoint;
@@ -296,7 +300,11 @@ static void mdss_dsi_panel_apply_settings(struct mdss_dsi_ctrl_pdata *ctrl,
 }
 
 
+#ifdef CONFIG_MACH_LONGCHEER
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+#else
 static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+#endif
 			struct dsi_panel_cmds *pcmds, u32 flags)
 {
 	struct dcs_cmd_req cmdreq;
@@ -497,6 +505,10 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
+
+#ifdef CONFIG_MACH_LONGCHEER
+	printk("%s, panel reset, enable = %d\n", __func__, enable);
+#endif
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1116,6 +1128,27 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 
 	switch (ctrl_pdata->bklt_ctrl) {
 	case BL_WLED:
+
+#ifdef CONFIG_MACH_LONGCHEER
+        if(bl_level != 0)
+		{
+              first_set_bl = true;
+	          if(mdss_first_set_feature(pdata, first_ce_state, first_cabc_state, first_srgb_state, first_gamma_state,
+						  first_cabc_movie_state, first_cabc_still_state))
+		            pr_err("%s first set feature fail ! \n", __func__);
+	            else{
+	               first_ce_state=-1;
+	               first_cabc_state=-1;
+	               first_srgb_state=-1;
+	               first_gamma_state=-1;
+			       first_cabc_movie_state=-1;
+				   first_cabc_still_state=-1;
+	            }
+           } else{
+                first_set_bl = false;
+            }
+#endif
+
 #ifndef CONFIG_BACKLIGHT_QCOM_SPMI_WLED
 		led_trigger_event(bl_led_trigger, bl_level);
 #else
@@ -1195,11 +1228,26 @@ static void panel_dead_report_delayed_work(struct work_struct *work)
 }
 #endif
 
+#ifdef CONFIG_MACH_LONGCHEER
+extern int ce_state;
+extern int cabc_state;
+extern int srgb_state;
+extern int cabc_movie_state;
+extern int cabc_still_state;
+#endif
+
 static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 {
 	struct mdss_dsi_ctrl_pdata *ctrl = NULL;
 	struct mdss_panel_info *pinfo;
 	struct dsi_panel_cmds *on_cmds;
+#ifdef CONFIG_MACH_LONGCHEER
+	struct dsi_panel_cmds *ce_on_cmds;
+	struct dsi_panel_cmds *srgb_on_cmds;
+	struct dsi_panel_cmds *cabc_on_cmds;
+	struct dsi_panel_cmds *cabc_movie_on_cmds;
+	struct dsi_panel_cmds *cabc_still_on_cmds;
+#endif
 	int ret = 0;
 
 	if (pdata == NULL) {
@@ -1220,6 +1268,14 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	on_cmds = &ctrl->on_cmds;
 
+#ifdef CONFIG_MACH_LONGCHEER
+	cabc_on_cmds = &ctrl->cabc_on_cmds;
+	ce_on_cmds = &ctrl->ce_on_cmds;
+	srgb_on_cmds = &ctrl->srgb_on_cmds;
+	cabc_movie_on_cmds = &ctrl->cabc_movie_on_cmds;
+	cabc_still_on_cmds = &ctrl->cabc_still_on_cmds;
+#endif
+
 	if ((pinfo->mipi.dms_mode == DYNAMIC_MODE_SWITCH_IMMEDIATE) &&
 			(pinfo->mipi.boot_mode != pinfo->mipi.mode))
 		on_cmds = &ctrl->post_dms_on_cmds;
@@ -1229,6 +1285,32 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	if (on_cmds->cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, on_cmds, CMD_REQ_COMMIT);
+
+#ifdef CONFIG_MACH_LONGCHEER
+	if(ce_state==14){
+	   if (ce_on_cmds->cmd_cnt)
+	       mdss_dsi_panel_cmds_send(ctrl,ce_on_cmds, CMD_REQ_COMMIT);
+	}
+	if(11 == srgb_state){
+	   if (srgb_on_cmds->cmd_cnt)
+	       mdss_dsi_panel_cmds_send(ctrl,srgb_on_cmds, CMD_REQ_COMMIT);
+	}
+	if(11 == cabc_state){
+		if (cabc_on_cmds->cmd_cnt)
+	       mdss_dsi_panel_cmds_send(ctrl,cabc_on_cmds, CMD_REQ_COMMIT);
+	}
+	if(cabc_movie_state == 1){
+		if (cabc_movie_on_cmds->cmd_cnt)
+	       mdss_dsi_panel_cmds_send(ctrl,cabc_movie_on_cmds, CMD_REQ_COMMIT);
+		pr_info("set cabc movie on\n");
+	}
+
+	if(cabc_still_state == 1){
+		if (cabc_still_on_cmds->cmd_cnt)
+	       mdss_dsi_panel_cmds_send(ctrl,cabc_still_on_cmds, CMD_REQ_COMMIT);
+		pr_info("set cabc still on\n");
+	}
+#endif
 
 #ifdef CONFIG_MACH_MI
 	if (pinfo->panel_dead && pinfo->initial_esd_check.check_cmd
@@ -3329,6 +3411,95 @@ static int mdss_panel_parse_dt(struct device_node *np,
 
 	rc = of_property_read_u32(np, "qcom,adjust-timer-wakeup-ms", &tmp);
 	pinfo->adjust_timer_delay_ms = (!rc ? tmp : 0);
+
+#ifdef CONFIG_MACH_LONGCHEER
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->ce_on_cmds,
+		"qcom,mdss-dsi-ce-on-command", "qcom,mdss-dsi-ce-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->ce_off_cmds,
+		"qcom,mdss-dsi-ce-off-command", "qcom,mdss-dsi-ce-off-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->srgb_on_cmds,
+		"qcom,mdss-dsi-srgb-on-command", "qcom,mdss-dsi-srgb-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->srgb_off_cmds,
+		"qcom,mdss-dsi-srgb-off-command", "qcom,mdss-dsi-srgb-off-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->cabc_on_cmds,
+		"qcom,mdss-dsi-cabc-on-command", "qcom,mdss-dsi-cabc-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->cabc_off_cmds,
+		"qcom,mdss-dsi-cabc-off-command", "qcom,mdss-dsi-cabc-off-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->cabc_movie_on_cmds,
+		"qcom,mdss-dsi-cabc-movie-on-command", "qcom,mdss-dsi-cabc-movie-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->cabc_still_on_cmds,
+		"qcom,mdss-dsi-cabc-still-on-command", "qcom,mdss-dsi-cabc-still-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm_off_cmds,
+		"qcom,mdss-dsi-hbm-off-command", "qcom,mdss-dsi-hbm-off-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm1_on_cmds,
+		"qcom,mdss-dsi-hbm1-on-command", "qcom,mdss-dsi-hbm1-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm2_on_cmds,
+		"qcom,mdss-dsi-hbm2-on-command", "qcom,mdss-dsi-hbm2-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm3_on_cmds,
+		"qcom,mdss-dsi-hbm3-on-command", "qcom,mdss-dsi-hbm3-on-command-state");
+
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma0_cmds,
+		"qcom,mdss-dsi-gamma0-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma1_cmds,
+		"qcom,mdss-dsi-gamma1-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma2_cmds,
+		"qcom,mdss-dsi-gamma2-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma3_cmds,
+		"qcom,mdss-dsi-gamma3-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma4_cmds,
+		"qcom,mdss-dsi-gamma4-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma5_cmds,
+		"qcom,mdss-dsi-gamma5-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma6_cmds,
+		"qcom,mdss-dsi-gamma6-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma7_cmds,
+		"qcom,mdss-dsi-gamma7-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma8_cmds,
+		"qcom,mdss-dsi-gamma8-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma9_cmds,
+		"qcom,mdss-dsi-gamma9-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma10_cmds,
+		"qcom,mdss-dsi-gamma10-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma11_cmds,
+		"qcom,mdss-dsi-gamma11-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma12_cmds,
+		"qcom,mdss-dsi-gamma12-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma13_cmds,
+		"qcom,mdss-dsi-gamma13-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma14_cmds,
+		"qcom,mdss-dsi-gamma14-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma15_cmds,
+		"qcom,mdss-dsi-gamma15-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma16_cmds,
+		"qcom,mdss-dsi-gamma16-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma17_cmds,
+		"qcom,mdss-dsi-gamma17-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma18_cmds,
+		"qcom,mdss-dsi-gamma18-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma19_cmds,
+		"qcom,mdss-dsi-gamma19-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma20_cmds,
+		"qcom,mdss-dsi-gamma20-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma21_cmds,
+		"qcom,mdss-dsi-gamma21-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma22_cmds,
+		"qcom,mdss-dsi-gamma22-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma23_cmds,
+		"qcom,mdss-dsi-gamma23-command", "qcom,mdss-dsi-gamma-command-state");
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->gamma24_cmds,
+		"qcom,mdss-dsi-gamma24-command", "qcom,mdss-dsi-gamma-command-state");
+#endif
 
 	pinfo->mipi.force_clk_lane_hs = of_property_read_bool(np,
 		"qcom,mdss-dsi-force-clock-lane-hs");
